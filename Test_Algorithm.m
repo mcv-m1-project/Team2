2f482286651b7a917cac4e25da27202c69acc3aa
LidiaGarrucho <lidiaxu.3@gmail.com>
2016-10-10 20:47:38 +0200

    2D Histograms ab

--- Test_Algorithm.m
+++ Test_Algorithm.m
@@ -10,12 +10,40 @@ dirbase = pwd;
 % Path to the training dataset images
 dirTrainDataSet = [dirbase, '\..\train'];
 
+% Path to Ground Truth annotations:
+dirgt = [dirTrainDataSet, '\gt'];
+% Path to Masks:
+dirmask = [dirTrainDataSet, '\mask'];
+
+% Create lists with the ground truth annotations files, the mask files, and
+% the original image files:
+[gt_list, mask_list, images_list] = create_files_list(dirTrainDataSet);
+
+% Counting of signals types in annotations in dirgt:
+[signals_list, nrepetitions] = count_signals_types(dirgt, gt_list);
+
+% Statistics about form factor:
+form_factor = analyze_form_factor(dirgt, gt_list);
+
+% Statistics about filling ratio:
+filling_ratio = analyze_filling_ratio(dirgt, gt_list, dirmask, mask_list);
+
+% Names of signals found:
+fprintf('Names of signals found:\n')
+signals_list
+
+% Times each signal appears:
+fprintf('Times each signal appears:\n')
+nrepetitions
+
+[trainSet, validationSet] = train_validation_split(dirTrainDataSet, nrepetitions);
+
 %Every time an image is display it will wait for mouse click or enter to
 %display the next one.
+%week1_task3_color_segmentation_test( dirTrainDataSet, trainSet)
+close all,
+week1_task3_color_segmentation_equalization_test(dirTrainDataSet, trainSet)
 
-%week1_task3_color_segmentation_test( dirTrainDataSet );
-
-week1_task3_color_segmentation_equalization_test( dirTrainDataSet );
 
 
 
--- week1_task3_color_segmentation_equalization_test.m
+++ week1_task3_color_segmentation_equalization_test.m
@@ -1,157 +1,110 @@
-function week1_task3_color_segmentation_equalization_test( dirTrainDataSet )
+function week1_task3_color_segmentation_equalization_test(dirTrainDataSet, trainSet)
 %week1_task3_color_segmentation_test
 %   Use Lab color space to make the color mask.
 %
 %   Parameters
 %       'dirTrainDataSet' - Path to the training dataset
-
-% Path to Ground Truth annotations:
-dirgt = [dirTrainDataSet, '\gt'];
-% Path to Masks:
-dirmask = [dirTrainDataSet, '\mask'];
-
-% Create lists with the ground truth annotations files, the mask files, and
-% the original image files:
-[gt_list, mask_list, images_list] = create_files_list(dirTrainDataSet);
-% Counting of signals types in annotations in dirgt:
-[signals_list, nrepetitions] = count_signals_types(dirgt, gt_list);
-
-% Names of signals found:
-fprintf('Names of signals found:\n')
-signals_list
-
-% Times each signal appears:
-fprintf('Times each signal appears:\n')
-nrepetitions
-
-[trainSet, validationSet] = train_validation_split(dirTrainDataSet, nrepetitions);
+%       'trainSet' - Array of images used for training
 
 %create a max and a min for each channel for each type of signal, in order to get the
 %color limits of each channels
 
 %type of signal A
-color_signal_max_ch1 = 0; %pivot values, it is going to change
-color_signal_min_ch1 = 1000; %pivot values, it is going to change
-color_signal_max_ch2 = 0; 
-color_signal_min_ch2 = 1000;
-color_signal_max_ch3 = 0; 
-color_signal_min_ch3 = 1000;
 
- 
 disp('Press Enter or Button click to display the next image:')
-figure,
+% figure,
 %Read train split images
-for idx = 1:size(trainSet,2)
-    
+hist_a_acc=zeros(64,64);
+hist_b_acc=zeros(64,64);
+hist_c_acc=zeros(64,64);
+hist_d_acc=zeros(64,64);
+hist_e_acc=zeros(64,64);
+hist_f_acc=zeros(64,64);
+
+counter_type_a = 0;
+for idx = 1:size(trainSet,2) 
     %Just for type sign A
-    %{
-    filePath = strcat('..\train\gt\gt.' trainSet{idx} '.txt');
-        [unused signs] = LoadAnnotations(filePath);
-    im_test = imread(['..\train\' trainSet{idx} '.jpg']);
-    im_mask = imread(['..\train\mask\mask.' trainSet{idx} '.png']);
-    %}
+    [~, signs_type] = LoadAnnotations([dirTrainDataSet '\gt\gt.' trainSet{idx} '.txt']);
     
-    %Histogram Equalization
-    r=im_test(:,:,1);
-    g=im_test(:,:,2);
-    b=im_test(:,:,3);
-    r1 = histeq(r);
-    g1 = histeq(g);
-    b1 = histeq(b);
-    im_test = cat(3,r1,g1,b1);
-    %imshow(im_test_eq);
-    
-    %im_test = double(im_test)/255;        % Cast to double in the range [0,1]
-    
-    %Convert to another color space
-    im_test = colorspace('HSV<-RGB',im_test);
-    
-    %compare if
-    im_test_signal = bsxfun(@times, im_test, cast(im_mask,class(im_test)));
-   
-    
-    
-    
-    ch1 = im_test_signal(:,:,1);
-    current_max_ch1 = max(max(ch1(ch1~=0)));
-    current_min_ch1 = min(min(ch1(ch1~=0)));
-    if(color_signal_max_ch1<current_max_ch1)
-        color_signal_max_ch1 = current_max_ch1;
-    end
-    
-    if(color_signal_min_ch1<current_min_ch1)
-        color_signal_min_ch1 = current_min_ch1;
-    end
-    
-    ch2 = im_test_signal(:,:,2);
-    current_max_ch2 = max(max(ch2(ch2~=0)));
-    current_min_ch2 = min(min(ch2(ch2~=0)));
-    if(color_signal_max_ch2<current_max_ch2)
-        color_signal_max_ch2 = current_max_ch2;
-    end
-    
-    if(color_signal_min_ch2<current_min_ch2)
-        color_signal_min_ch2 = current_min_ch2;
-    end
-    
-    ch3 = im_test_signal(:,:,3);
-    current_max_ch3 = max(max(ch3(ch3~=0)));
-    current_min_ch3 = min(min(ch3(ch3~=0)));
-    if(color_signal_max_ch3<current_max_ch3)
-        color_signal_max_ch3 = current_max_ch3;
-    end
-    
-    if(color_signal_min_ch3<current_min_ch3)
-        color_signal_min_ch3 = current_min_ch3;
-    end
-    
-    
+        im_test = imread([dirTrainDataSet '\' trainSet{idx} '.jpg']);
+        im_mask = imread([dirTrainDataSet '\mask\mask.' trainSet{idx} '.png']);
+        
+        im_test = double(im_test)/255;        % Cast to double in the range [0,1]
+        %Convert to another color space
+        im_test = colorspace('Lab<-RGB',im_test);
+        %compare if
+        im_test_signal = bsxfun(@times, im_test, cast(im_mask,class(im_test)));
+
+        ch_a = im_test_signal(:,:,2);
+        ch_b = im_test_signal(:,:,3);
+           
+        h = hist3(double([ch_a(:) ch_b(:)]), [64 64]);
+        [m idx] = max(h(:));
+        h(idx) = 0;
+        switch signs_type{1}
+            case 'A'
+             hist_a_acc = hist_a_acc + h;
+            case 'B'
+             hist_b_acc = hist_b_acc + h;
+            case 'C'
+             hist_c_acc = hist_c_acc + h;
+            case 'D'
+             hist_d_acc = hist_d_acc + h;
+            case 'E'
+             hist_e_acc = hist_e_acc + h;
+            case 'F'
+             hist_f_acc = hist_f_acc + h;
+        end
 end
-color_signal_max_ch1
-color_signal_max_ch2
-color_signal_max_ch3
-color_signal_min_ch1
-color_signal_min_ch2
-color_signal_min_ch3
+figure, bar3(hist_a_acc), title('A'), xlabel('a component'), ylabel('b component')
+figure, bar3(hist_b_acc), title('B'), xlabel('a component'), ylabel('b component')
+figure, bar3(hist_c_acc), title('C'), xlabel('a component'), ylabel('b component')
+figure, bar3(hist_d_acc), title('D'), xlabel('a component'), ylabel('b component')
+figure, bar3(hist_e_acc), title('E'), xlabel('a component'), ylabel('b component')
+figure, bar3(hist_f_acc), title('F'), xlabel('a component'), ylabel('b component')
+
 
 for idx = 1:size(trainSet,2)
     im_test = imread(['..\train\' trainSet{idx} '.jpg']);
     %Histogram Equalization
-    r=im_test(:,:,1);
-    g=im_test(:,:,2);
-    b=im_test(:,:,3);
-    r1 = histeq(r);
-    g1 = histeq(g);
-    b1 = histeq(b);
-    im_test = cat(3,r1,g1,b1);
-    
+%     r=im_test(:,:,1);
+%     g=im_test(:,:,2);
+%     b=im_test(:,:,3);
+%     r1 = histeq(r);
+%     g1 = histeq(g);
+%     b1 = histeq(b);
+%     im_test = cat(3,r1,g1,b1);
+    im_test = double(im_test)/255;        % Cast to double in the range [0,1]
     % Change color space and plot it
    
     subplot(2,2,1);
     image(im_test);
     axis image
+    title Mask
     %Change colorspace
-    B = colorspace('HSV<-RGB',im_test); 
+    B = colorspace('Lab<-RGB',im_test); 
     %Plot every channel separately
     % View the individual channels
     subplot(2,2,2);
     imagesc(B(:,:,1));
     colormap(gray(256));
     axis image
-    title L
+    title H
     subplot(2,2,3);
     imagesc(B(:,:,2));
     colormap(gray(256));
     axis image
-    title a
+    title S
     subplot(2,2,4);
     imagesc(B(:,:,3));
     colormap(gray(256));
     axis image
-    title b
+    title V
     
-%channel 1    
-C = C | (B(:,:,1) > color_signal_min_ch1 & B(:,:,1) < color_signal_max_ch1 & B(:,:,2) > color_signal_min_ch2 & B(:,:,2) < color_signal_max_ch1 & B(:,:,3) > color_signal_min_ch3 & B(:,:,3) < color_signal_max_ch1);
+    B(:,:,2) = histeq( B(:,:,2),256);
+    B(:,:,3) = histeq( B(:,:,3),256);
+%Signal type A   
+C = (B(:,:,2) >= color_signal_min_ch1 & B(:,:,2) <= color_signal_max_ch1 & B(:,:,3) >= color_signal_min_ch2 & B(:,:,3) <= color_signal_max_ch2 );
 subplot(2,2,1), imshow(C)
 
 w = waitforbuttonpress;
