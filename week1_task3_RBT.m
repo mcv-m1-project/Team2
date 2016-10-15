e59e52c83f7e175ef23d01e4b28b72d5dd997bdd
xianlopez <lopezalvarez.xian@gmail.com>
2016-10-15 18:25:34 +0200

    Write RBT
    Write RBT masks of the validation set.

--- week1_task3_RBT.m
+++ week1_task3_RBT.m
@@ -23,36 +23,27 @@ load('signals_workspace.mat');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%% Must be run only ONCE %%%%%%%%%%%%%%%%%%%%%
-ngrid = 20;
-RBT_prepare(trainSet, signals, image_list, dirTrainDataSet, mask_list, dirmask, ngrid)
+% ngrid = 20;
+% RBT_prepare(trainSet, signals, image_list, dirTrainDataSet, mask_list, dirmask, ngrid)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% To fun before a loop computing the masks for every image %%%
-min_percenin_blue = 50;
-min_percenin_red = 40;
-% This selects the thresholds that maximize the ratio and resepct the
-% minimum percentage especified for pixels found in signals. Then it writes
-% this thresholds to disk:
-RBT_select(min_percenin_blue, min_percenin_red)
+%%%% To run when tuning parameters of RBT need to be changed %%%%
+% min_percenin_blue = 50;
+% min_percenin_red = 40;
+% % This selects the thresholds that maximize the ratio and resepct the
+% % minimum percentage especified for pixels found in signals. Then it writes
+% % this thresholds to disk:
+% RBT_select(min_percenin_blue, min_percenin_red)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-%Every time an image is display it will wait for mouse click or enter to
-%display the next one.
-figure, 
-for idx = 1:size(validationSet,2)
-    im_test = imread([dirTrainDataSet '\' validationSet{idx} '.jpg']);
-    image_lab = rgb2lab(im_test);
-    mask = RBT_mask(im_test);
-   
-    subplot(1,2,1), imshow(im_test)  
-    subplot(1,2,2), imshow(mask, [0 1])
-
-    w = waitforbuttonpress;
+% Write masks for every image of the validation set:
+for file = 1:length(validationSet)
+    image = imread([dirTrainDataSet '\' validationSet{file} '.jpg']);
+    mask = RBT_mask(image);
+    imwrite(mask, [dirTrainDataSet '\result_mask\RBT\maskRBT_' validationSet{file} '.png']);
 end
 
 
