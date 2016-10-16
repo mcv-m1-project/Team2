a444eacb618c097237fbebed32e86b7d6a18297f
XeniaSalinas <salinasxenia@gmail.com>
2016-10-16 18:24:10 +0200

    add comments

--- week1_task3_signal_types_2D_ab_histograms.m
+++ week1_task3_signal_types_2D_ab_histograms.m
@@ -24,11 +24,8 @@ counter_type_e = 0;
 counter_type_f = 0;
 
 for idx = 1:size(trainSet,2) 
-
-%     [~, signs_type] = LoadAnnotations([dirTrainDataSet '\gt\gt.' trainSet{idx} '.txt']);
-%     im_test = imread([dirTrainDataSet '\' trainSet{idx} '.jpg']);
-%     im_mask = imread([dirTrainDataSet '\mask\mask.' trainSet{idx} '.png']);
-
+    
+    %Compute the joint histograms for each signal type
     im_index = find( strcmp({signals(:).filename},trainSet{idx}));
     if (isempty(im_index) == 0)
         %Each image can contain more than one signal
@@ -47,12 +44,7 @@ for idx = 1:size(trainSet,2)
             
             ch_a = im_test_signal(:,:,2);
             ch_b = im_test_signal(:,:,3);
-            
-            %Show images
-%             subplot(3,1,1), imshow(im_test_signal)
-%             subplot(3,1,2), imshow(ch_a, [])
-%             subplot(3,1,3), imshow(ch_b, [])
-            
+                      
             h = hist3(double([ch_a(:) ch_b(:)]), [64 64]);
             % [m idx] = max(h(:));
             % h(idx) = 0;
@@ -79,6 +71,7 @@ for idx = 1:size(trainSet,2)
         end
     end
 end
+%normalize the joint histograms
 hist_a_acc = hist_a_acc./counter_type_a;
 hist_b_acc = hist_b_acc./counter_type_b;
 hist_c_acc = hist_c_acc./counter_type_c;
@@ -107,15 +100,18 @@ hist_f_acc = hist_f_acc./counter_type_f;
 % subplot(2,3,5), imagesc(hist_e_acc), title('E'), xlabel('a component'), ylabel('b component')
 % subplot(2,3,6), imagesc(hist_f_acc), title('F'), xlabel('a component'), ylabel('b component')
 
+%Obtain the global join histogram
 hist_acc = hist_a_acc + hist_b_acc + hist_c_acc + hist_d_acc + hist_e_acc + hist_f_acc;
 % figure, bar3(hist_acc), title('global'), xlabel('a component'), ylabel('b component')
 
+%Find the two most representitive colors (red and blue)
 first_max = max(max(hist_acc));
 [first_max_i,first_max_j] = find (hist_acc==first_max);
 second_max = max(max(hist_acc(hist_acc < first_max)));
 [second_max_i,second_max_j] = find (hist_acc==second_max);
-hist_filter = zeros(64,64);
 
+%Compute the filter that is going to be used
+hist_filter = zeros(64,64);
 rad_blue = 13;
 for i = max(first_max_i-rad_blue,1):min(first_max_i+rad_blue,64)
     for j = max(first_max_j-rad_blue,1):min(first_max_j+rad_blue,64)
@@ -135,9 +131,11 @@ save('2d_ab_histograms_filter', 'hist_acc_filtered');
 
 toc
 
+%Obtain the masks for each image on the training set
 [unused,trainSize] = size(trainSet);
 for image = 1:trainSize
    im_orig = imread([dirTrainDataSet '\' trainSet{image} '.jpg']);
+   %Change to Lab colorspace
    im = double(im_orig)/255;
    im = colorspace('Lab<-RGB',im);
    [height,width,unused] = size(im);
@@ -145,8 +143,10 @@ for image = 1:trainSize
    [m,n,unused] = size(im);
    for i = 1:m
        for j = 1:n
+           %Find the histogram bin where the pixel color is represented
            a_component = ceil((im(i,j,2)+128)/4);
            b_component = ceil((im(i,j,3)+128)/4);
+           %Decide if that pixel is part of a traffic signal or not
            if hist_acc_filtered(a_component,b_component) > 0
                mask(i,j) = 1;
            end
@@ -155,6 +155,7 @@ for image = 1:trainSize
 %    figure,
 %    subplot(1,2,1), imshow(im_orig);
 %    subplot(1,2,2), imshow(mask, [0 1]);
+   %Save the obtained mask
    imwrite(mask,[dirTrainDataSet '\result_mask\2d_hist\train\' trainSet{image} '.png']);
 end
 
--- week1_task3_signal_types_2D_ab_histograms_validation.m
+++ week1_task3_signal_types_2D_ab_histograms_validation.m
@@ -6,14 +6,15 @@ function week1_task3_signal_types_2D_ab_histograms_validation(dirTrainDataSet, v
 %       'dirTrainDataSet' - Path to the validation dataset
 %       'validationSet' - Array of images used for validation
 
-% Initialize histograms and counters:
 tic
+%Load the filter obtained on the training step
 hist_acc_filtered = load('2d_ab_histograms_filter');
 hist_acc_filtered = hist_acc_filtered.hist_acc_filtered;
 
 [unused,validationSize] = size(validationSet);
 for image = 1:validationSize
    im_orig = imread([dirTrainDataSet '\' validationSet{image} '.jpg']);
+   %Change the colorspace to Lab
    im = double(im_orig)/255;
    im = colorspace('Lab<-RGB',im);
    [height,width,unused] = size(im);
@@ -21,8 +22,10 @@ for image = 1:validationSize
    [m,n,unused] = size(im);
    for i = 1:m
        for j = 1:n
+           %Find the histogram bin where the pixel color is represented
            a_component = ceil((im(i,j,2)+128)/4);
            b_component = ceil((im(i,j,3)+128)/4);
+           %Decide if that pixel is part of a traffic signal or not
            if hist_acc_filtered(a_component,b_component) > 0
                mask(i,j) = 1;
            end
@@ -31,6 +34,7 @@ for image = 1:validationSize
 %    figure,
 %    subplot(1,2,1), imshow(im_orig);
 %    subplot(1,2,2), imshow(mask, [0 1]);
+   %Save the obtained mask
    imwrite(mask,[dirTrainDataSet '\result_mask\2d_hist\validation\' validationSet{image} '.png']);
 end
 
