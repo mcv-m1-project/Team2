7a6c4968a8ae5cfd8406f6c79a1788663759fc24
XeniaSalinas <salinasxenia@gmail.com>
2016-10-16 18:44:57 +0200

    Week1 delivery

---
+++ Week1/Color_segmentation.m
@@ -0,0 +1,46 @@
+clear all
+close all
+% We add the path where some scripts are.
+addpath('evaluation\')
+addpath('colorspace\')
+addpath('..\test\')
+
+% Base directory:
+dirbase = pwd;
+% Path to the test dataset images
+dirTestDataSet = [dirbase, '\..\test'];
+
+% Path to the computed masks:
+dir_result_masks = [dirTestDataSet, '\result_masks'];
+
+%Load variables from week1_task1 to save computation time
+load('signals_workspace.mat');
+
+%Load the threshold needed in the 2D joint histogram method
+hist_acc_filtered = load('2d_ab_histograms_filter');
+
+%Compute the masks for the test images
+test_images = dir(dirTestDataSet);
+tic
+hist_acc_filtered = hist_acc_filtered.hist_acc_filtered;
+for idx = 3:size(test_images,1)-1
+   im_orig = imread([dirTestDataSet '\' test_images(idx).name]);
+   im = double(im_orig)/255;
+   im = colorspace('Lab<-RGB',im);
+   [height,width,unused] = size(im);
+    mask = zeros(height,width);
+   [m,n,unused] = size(im);
+   for i = 1:m
+       for j = 1:n
+           a_component = ceil((im(i,j,2)+128)/4);
+           b_component = ceil((im(i,j,3)+128)/4);
+           if hist_acc_filtered(a_component,b_component) > 0
+               mask(i,j) = 1;
+           end
+       end
+   end
+   file_name = strsplit(test_images(idx).name,'.jpg');
+   imwrite(mask,[dirTestDataSet '\result_masks\mask.' char(file_name(1)) '.png']);
+end
+
+toc
---
+++ Week1/week1_task3_RBT.m
@@ -0,0 +1,35 @@
+function week1_task3_RBT(dirTrainDataSet, trainSet, validationSet, signals)
+%week1_task3_color_segmentation_test
+%   Use Lab color space to make the color mask.
+%
+%   Parameters
+%       'dirTrainDataSet' - Path to the training dataset
+%       'trainSet' - Array of images used for training
+%       'signals' - Array of objects with all the information about each signal
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%% Must be run only ONCE %%%%%%%%%%%%%%%%%%%%%
+% ngrid = 20;
+% RBT_prepare(trainSet, signals, image_list, dirTrainDataSet, mask_list, dirmask, ngrid)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% To run when tuning parameters of RBT need to be changed %%%%
+% min_percenin_blue = 50;
+% min_percenin_red = 40;
+% % This selects the thresholds that maximize the ratio and resepct the
+% % minimum percentage especified for pixels found in signals. Then it writes
+% % this thresholds to disk:
+% RBT_select(min_percenin_blue, min_percenin_red)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Write masks for every image of the validation set:
+for file = 1:length(validationSet)
+    image = imread([dirTrainDataSet '\' validationSet{file} '.jpg']);
+    mask = RBT_mask(image);
+    imwrite(mask, [dirTrainDataSet '\result_mask\RBT\validation\' validationSet{file} '.png']);
+end
+
+end
+
