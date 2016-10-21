6b206dd76db1ae9f01927aae87a317f93ca47bf9
LidiaGarrucho <lidiaxu.3@gmail.com>
2016-10-21 09:53:49 +0200

    Little change to clean big white objects

--- Week2/apply_morphological_operators.m
+++ Week2/apply_morphological_operators.m
@@ -9,10 +9,15 @@ function out_mask = apply_morphological_operators(in_mask)
 
 %subplot(2,2,1), imshow(in_mask, [0,1]), title('input mask')
 
+%Binary open of a 30000 pixel region to clean very big areas (larger than max signal size)
+out_mask = bwareaopen(in_mask, 30000);
+out_mask = in_mask - out_mask;
+%subplot(2,2,2), imshow(out_mask, [0,1]), title('bwareaopen 53000')
+
 %Apply imclose first to define better the signal borders
 se = strel('square', 5);
-out_mask = imclose(in_mask, se);
-%subplot(2,2,2), imshow(out_mask, [0,1]), title('imclose 10')
+out_mask = imclose(out_mask, se);
+%subplot(2,2,3), imshow(out_mask, [0,1]), title('imclose 10')
 
 %Fill image holes
 out_mask = imfill(out_mask,'holes');
