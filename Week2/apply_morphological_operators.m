e2ba6b4117e0f117cfdc39269f736dd81b6960a9
LidiaGarrucho <lidiaxu.3@gmail.com>
2016-10-23 17:42:36 +0200

    Last changes to filter undesired elements

--- Week2/apply_morphological_operators.m
+++ Week2/apply_morphological_operators.m
@@ -1,38 +1,37 @@
-function out_mask = apply_morphological_operators(in_mask, min_size, max_size)
+function out_mask = apply_morphological_operators(in_mask, signals_size)
 %apply_morphological_operators
 %   Apply morphological operators to the input mask
 %
 %   Parameters
 %       'in_mask' - Input mask image
-%       'min_size' - Minimum signal size
-%       'max_size' - Maximum signal size
+%       'signals_size' - Signal size parameters
 %   Return
 %       'out_mask' - Output mask image
 
-subplot(2,2,1), imshow(in_mask, [0,1]), title('input mask')
+%subplot(2,2,1), imshow(in_mask, [0,1]), title('input mask')
 
 %Apply imclose first to define better the signal borders
 se = strel('square', 5);
 out_mask = imclose(in_mask, se);
-subplot(2,2,2), imshow(out_mask, [0,1]), title('imclose 5')
 
 %Fill image holes
 out_mask = imfill(out_mask,'holes');
-subplot(2,2,3), imshow(out_mask, [0,1]), title('fill holes')
-
-%Low threshold to clean very small areas (smaller than min signal size/2)
-low_threshold = floor(min(min_size)/2);
-
-%Upper threshold to clean very big areas (larger than max signal size)
-up_threshold = floor(max(max_size));
+%subplot(2,2,2), imshow(out_mask, [0,1]), title('fill holes')
 
 %Apply and area opening for the binary mask to the small and big areas that
 %cannot be traffic signals
-out_mask = bwareaopen(out_mask, low_threshold);
+% out_mask = bwareaopen(out_mask, signals_size.min_area);
+% aux_out_mask = bwareaopen(out_mask, signals_size.max_area);
+% out_mask = out_mask - aux_out_mask;
+% subplot(2,2,3), imshow(out_mask, [0,1]), title('bwareaopen')
 
-aux_out_mask = bwareaopen(out_mask, up_threshold);
-out_mask = out_mask - aux_out_mask;
+el = strel('rectangle', floor([signals_size.min_height signals_size.min_width]));
+aux_out_mask_min = imopen(out_mask, el);
 
-subplot(2,2,4), imshow(out_mask, [0,1]), title('bwareaopen')
+el = strel('rectangle', floor([signals_size.max_height signals_size.max_width]));
+aux_out_mask_max = imopen(out_mask, el);
+out_mask = aux_out_mask_min | aux_out_mask_max;
+
+%subplot(2,2,4), imshow(out_mask, [0,1]), title('imopen')
 
 end
--- Week2/week2_task_3_apply_morphological_operators.m
+++ Week2/week2_task_3_apply_morphological_operators.m
@@ -1,12 +1,14 @@
-function outMasksDir = week2_task_3_apply_morphological_operators(dirMask, dirGroundTruthMasks, min_size, max_size)
+function outMasksDir = week2_task_3_apply_morphological_operators(dirMask, dirGroundTruthMasks, min_size, max_size, form_factor, filling_ratio)
 %week2_task_3_apply_morphological_operators
 %   Use morphological operators to improve results on color segmentation
 %
 %   Parameters
 %       'dirMask' - Path to the input masks folder
 %       'dirGroundTruthMasks' - Path to the truth masks folder
-%       'min_size' - Minimum signal size
-%       'max_size' - Maximum signal size
+%       'min_size' - Minimum signal sizes
+%       'max_size' - Maximum signal sizes
+%       'form_factor' - Signals form factors
+%       'filling_ratio' - Signals filling ratio
 %   Return
 %       'outMasks' - Path to the output masks folder
 
@@ -20,11 +22,19 @@ if(7~=exist(outMasksDir,'dir'))
     mkdir(outMasksDir);
 end
 
+%Compute maximum and minimum width
+signals_size.min_area = floor(min(min_size)*min(filling_ratio));
+signals_size.max_area = floor(max(max_size)*max(filling_ratio));
+signals_size.max_width = sqrt(max(form_factor)*max(max_size));
+signals_size.min_width = sqrt(min(form_factor)*min(min_size));
+signals_size.max_height = sqrt(max(max_size)/min(form_factor));
+signals_size.min_height = sqrt(min(min_size)/max(form_factor));
+
 %figure,
 for idx = 1:size(input_masks,1)
    in_mask = imread([dirMask '\' input_masks(idx).name]);
    
-   out_mask = apply_morphological_operators(in_mask, min_size, max_size);
+   out_mask = apply_morphological_operators(in_mask, signals_size);
    
    imwrite(out_mask,[outMasksDir input_masks(idx).name]);
    
