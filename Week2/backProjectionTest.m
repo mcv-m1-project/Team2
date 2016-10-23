1d15d87c9c5c915d0203c31b793b6f32addd52d2
XeniaSalinas <salinasxenia@gmail.com>
2016-10-23 16:54:40 +0200

    bugs

--- Week2/backProjectionTest.m
+++ Week2/backProjectionTest.m
@@ -2,17 +2,12 @@ function backProjectionTest( dirTestDataSet)
 %BACKPROJECTIONTEST
 %Funtion that applys histogram back-projection to obtain the traffic signal masks
 %   Parameters
-%       'dirTestDataSet' - Path to the training set
-%       'testSet' - Vector containing the validation set (resulting from the train/validation split) identifiers
+%       'dirTestDataSet' - Path to the test set
 
 hist = load('back-projection-Lab');
 hist_g1 = hist.hist_g1;
 hist_g2 = hist.hist_g2;
 hist_g3 = hist.hist_g3;
-
-max_prob_g1 = max(max(hist_g1));
-max_prob_g2 = max(max(hist_g2));
-max_prob_g3 = max(max(hist_g3));
 
 th_g1=hist.th_g1;
 th_g2=hist.th_g2;
--- Week2/backProjectionTestHSV.m
+++ Week2/backProjectionTestHSV.m
@@ -1,27 +1,23 @@
-function backProjectionTestHSV( dirTestDataSet, testSet)
+function backProjectionTestHSV( dirTestDataSet)
 %BACKPROJECTIONTEST
 %Funtion that applys histogram back-projection to obtain the traffic signal masks
 %   Parameters
-%       'dirTestDataSet' - Path to the training set
-%       'testSet' - Vector containing the validation set (resulting from the train/validation split) identifiers
+%       'dirTestDataSet' - Path to the test set
 
 hist = load('back-projection-HSV');
 hist_g1 = hist.hist_g1;
 hist_g2 = hist.hist_g2;
 hist_g3 = hist.hist_g3;
 
-max_prob_g1 = max(max(hist_g1));
-max_prob_g2 = max(max(hist_g2));
-max_prob_g3 = max(max(hist_g3));
-
 th_g1=hist.th_g1;
 th_g2=hist.th_g2;
 th_g3=hist.th_g3;
 
-for image=1:length(testSet)
-	im_orig = imread([dirTestDataSet '\' testSet{image} '.jpg']);
-	[mask] = maskCalculationHSV(im_orig, hist_g1,hist_g2,hist_g3, th_g1, th_g2,th_g3);
-	imwrite(mask,[dirTrainDataSet '\result_mask\back-projection\HSV\' validationSet{image} '.png']);
+testSet = dir(dirTestDataSet);
+for image=3:length(testSet)
+	im_orig = imread([dirTestDataSet '\' testSet(image).name]);
+	[mask] = maskCalculation(im_orig, hist_g1,hist_g2,hist_g3, th_g1, th_g2,th_g3);
+	imwrite(mask,[dirTestDataSet '\result_masks\back-projectionHSV\validation\' testSet(image).name(1:9) '.png']);
 end
 
 end
