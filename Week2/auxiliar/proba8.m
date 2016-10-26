5b3075401d3fb0e626c33f6caff6ce747e050dff
xianlopez <lopezalvarez.xian@gmail.com>
2016-10-26 14:12:29 +0200

    Revert "mas sucio"
    This reverts commit 3e49ba8c0459782bc37c19a795e96ed2badf24b3.

--- Week2/auxiliar/proba8.m
+++ Week2/auxiliar/proba8.m
@@ -29,45 +29,12 @@ load('signals_workspace');
 %%%%%%  validation
 
 
-% For validating, we will take only a fraction of the validation set:
-percen_sets = 30;
-% Validation set:
-nvalidation = length(validationSet);
-tags = randsample(nvalidation, floor(nvalidation * percen_sets / 100));
-validation_red = cell(0);
-for i = 1:length(tags)
-    validation_red{i} = validationSet{tags(i)};
-end
-nvalidation = length(validation_red);
-
 % Loading parameters:
-
-%%%%%%%%%%%%%%%
-% Method choice:
-% method = 'mod2_3groups';
-method = 'RBT';
-
-% Loading parameters and defining run function:
-if(strcmp(method,'mod2_3groups'))
-    load('bp_mod3_params.mat')
-    runfun = @run_mod2_3groups;
-    
-elseif(strcmp(method,'RBT'))
-    load('RBT_thresholds_blue.mat')
-    load('RBT_thresholds_red.mat')
-    params.thresholds_blue = thresholds_blue;
-    params.thresholds_red = thresholds_red;
-    params.colorspace = 'lab';
-    runfun = @RBT_mask;
-    
-else
-    error('Method not recognized.')
-end
-
+load('bp_mod3_params.mat')
 
 % Computing validation:
 [precision, accuracy, recall, F1, TP, FP, FN, tpf] = ...
-    measure_method_efficiency(validation_red, dirimage, dirmask, runfun, params)
+    validation_mod2_3groups(validationSet, dirimage, dirmask, params)
 
 
 
