b50d34116c2e8bd76349b993d2c36f6f3779e613
xianlopez <lopezalvarez.xian@gmail.com>
2016-10-27 22:23:58 +0200

    Rellenar componentes conexas
    Las componentes conexas se pueden rellenar, y eso no depende de su
    tamaño. Puede ser muy útil para rellenar las señales.

--- Week3/example3.m
+++ Week3/example3.m
@@ -75,18 +75,47 @@ CCproperties = regionprops(CC, 'Area', 'BoundingBox');
 
 % Clean the mask eliminating the componets which bounding box has an area
 % lower than... min_area
-min_area = 200;
+% min_area = 200;
+% newCC = CC;
+% newCC.NumObjects = 0;
+% newCC.PixelIdxList = cell(0);
+% for i = 1:CC.NumObjects
+%     if(CCproperties(i).BoundingBox(3) * CCproperties(i).BoundingBox(4) > min_area)
+%         newCC.NumObjects = newCC.NumObjects + 1;
+%         newCC.PixelIdxList{newCC.NumObjects} = CC.PixelIdxList{i};
+%     end
+% end
+% newCCproperties = regionprops(newCC, 'Area', 'BoundingBox', 'FilledImage');
+
+% Clean the mask eliminating the componets which bounding box has any side
+% lower than... min_side
+min_side = 15;
 newCC = CC;
 newCC.NumObjects = 0;
 newCC.PixelIdxList = cell(0);
 for i = 1:CC.NumObjects
-    if(CCproperties(i).BoundingBox(3) * CCproperties(i).BoundingBox(4) > min_area)
+    if(CCproperties(i).BoundingBox(3) > min_side && CCproperties(i).BoundingBox(4) > min_side)
         newCC.NumObjects = newCC.NumObjects + 1;
         newCC.PixelIdxList{newCC.NumObjects} = CC.PixelIdxList{i};
     end
 end
+newCCproperties = regionprops(newCC, 'Area', 'BoundingBox', 'FilledImage', 'Image');
 
-newCCproperties = regionprops(newCC, 'Area', 'BoundingBox');
+% New mask without the discarded parts:
+cleanmask = zeros(size(mask));
+for i = 1:newCC.NumObjects
+    x = floor(newCCproperties(i).BoundingBox(1));
+    y = floor(newCCproperties(i).BoundingBox(2));
+    w = newCCproperties(i).BoundingBox(3);
+    h = newCCproperties(i).BoundingBox(4);
+    cleanmask(y+1:y+h, x+1:x+w) = newCCproperties(i).Image;
+end
+figure()
+subplot(1,2,1)
+imshow(mask, [0 1])
+subplot(1,2,2)
+imshow(cleanmask, [0 1])
+
 
 % Plot in the image the resulting bounding boxes:
 figure()
@@ -102,6 +131,22 @@ for i = 1:newCC.NumObjects
     plot([x x+w], [y y], 'y')
     plot([x x+w], [y+h y+h], 'y')
 end
+
+
+% Substitute the connected components for the filled versions:
+filledmask = mask;
+for i = 1:newCC.NumObjects
+    x = floor(newCCproperties(i).BoundingBox(1));
+    y = floor(newCCproperties(i).BoundingBox(2));
+    w = newCCproperties(i).BoundingBox(3);
+    h = newCCproperties(i).BoundingBox(4);
+    filledmask(y+1:y+h, x+1:x+w) = newCCproperties(i).FilledImage;
+end
+figure()
+subplot(1,2,1)
+imshow(mask, [0 1])
+subplot(1,2,2)
+imshow(filledmask, [0 1])
 
 
 
