f3e12c522cc378e725061c5ee0403f3a385c0648
XeniaSalinas <salinasxenia@gmail.com>
2016-10-29 14:57:36 +0200

    slidingWindow training
    train with different window sizes

--- Week3/slidingWindowTraining.m
+++ Week3/slidingWindowTraining.m
@@ -1,27 +1,45 @@
-function slidingWindowTraining(data_train,fillingFactor,minSize,maxSize)
+function [precisionVec,recallVec,F1Vec,widthVec,heightVec] = slidingWindowTraining(data_train,fillingFactor,minSize,maxSize)
 
 minWidth = min(round(sqrt(minSize.*fillingFactor)));
-minHigh = min(round(sqrt(minSize./fillingFactor)));
+minHeight = min(round(sqrt(minSize./fillingFactor)));
 
 maxWidth = max(round(sqrt(maxSize.*fillingFactor)));
-maxHigh = max(round(sqrt(maxSize./fillingFactor)));
+maxHeight = max(round(sqrt(maxSize./fillingFactor)));
 
 files = listFiles(data_train);
 nFiles = length(files);
 
-for i=1:nFiles
-    width = minWidth;
-    height = minHigh;
-    stepW = 5;
-    stepH = 5;
-    im = imread([data_train '\result_masks\morphological_operators\' strrep(files(i).name, '.jpg', '.png')]);
-    [mask, windowCandidates] = slidingWindow(im, width, height, stepW, stepH);
-    if(7~=exist([data_train, '\result_masks\slidingWindow\'],'dir'))
-        mkdir([data_train, '\result_masks\slidingWindow\']);
-    end
-    imwrite(mask,[data_train '\result_masks\slidingWindow\' strrep(files(i).name, '.jpg', '.png')]);
-    save([data_train '\result_masks\slidingWindow\' strrep(files(i).name, '.jpg', '.mat')],'windowCandidates');
-    [precision, recall, accuracy, F1, TP, FN, FP] = region_based_evaluation(dirGroundTruth, dirBBoxResults);
-end
+precisionVec = [];
+recallVec = [];
+F1Vec = [];
+widthVec = [];
+heightVec = [];
 
+for width=minWidth:10:maxWidth
+    for height=minHeight:10:maxHeight
+        stepW = round(0.2*width);
+        stepH = round(0.2*height);
+        
+        TP = 0;
+        FN = 0;
+        FP = 0;
+        for i=1:nFiles
+            im = imread([data_train '\result_masks\morphological_operators\' strrep(files(i).name, '.jpg', '.png')]);
+            [mask, windowCandidates] = slidingWindowImage(im, width, height, stepW, stepH);
+            [annotations Signs] = LoadAnnotations([data_train '\gt\gt.' fileId '.txt']);
+            [localTP,localFN,localFP] = PerformanceAccumulationWindow(windowCandidates, annotations);
+            TP = TP + localTP;
+            FN = FN + localFN;
+            FP = FP + localFP;
+        end
+        [precision, recall, accuracy] = PerformanceEvaluationWindow(TP, FN, FP)
+        F1 = 2*precision*recall/(precision+recall);
+        
+        precisionVec = [precisionVec precision];
+        recallVec = [recallVec recall];
+        F1Vec = [F1Vec F1];
+        widthVec = [widthVec width];
+        heightVec = [heightVec height];
+    end
+end
 end
