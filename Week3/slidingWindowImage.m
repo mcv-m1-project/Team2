bc0b9466fec6b2f525ba054091f242f6727f37ba
XeniaSalinas <salinasxenia@gmail.com>
2016-10-29 16:31:51 +0200

    bugs resolution

--- Week3/slidingWindowImage.m
+++ Week3/slidingWindowImage.m
@@ -39,42 +39,40 @@ end
 %     if(new == 1)
 %         windowCandidates = [windowCandidates windows(winPos)];
 %     end
-% end  
+% end
 
 %Delete overlapped detections (union)
 imPos = 1:M*N;
 imPos = reshape(imPos,[N,M]);
-windowCandidates = windows(1);
-for winPos=2:length(windows)
-    new = 1;
-    for winDef=1:length(windowCandidates)
-        rectPos = imPos(windows(winPos).y:windows(winPos).y+windows(winPos).h-1,windows(winPos).x:windows(winPos).x+windows(winPos).w-1);
-        rectDef = imPos(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1);
-        int = intersect(rectPos,rectDef);
-        if 0==isempty(int) %No empty means that both evaluated windows are overlapped
-            new = 0;
-            uni = union(rectPos,rectDef);
-            [minY,minX] = find(imPos==min(uni));
-            [maxY,maxX] = find(imPos==max(uni));
-            winFin = struct('x',minX,'y',minY,'w',maxX-minX+1,'h',maxY-minY+1);
-            windowCandidates(winDef)=winFin;
-            break;
+windowCandidates = [];
+if(0==isempty(windows))
+    windowCandidates = windows(1);
+    for winPos=2:length(windows)
+        new = 1;
+        for winDef=1:length(windowCandidates)
+            rectPos = imPos(windows(winPos).y:windows(winPos).y+windows(winPos).h-1,windows(winPos).x:windows(winPos).x+windows(winPos).w-1);
+            rectDef = imPos(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1);
+            int = intersect(rectPos,rectDef);
+            if 0==isempty(int) %No empty means that both evaluated windows are overlapped
+                new = 0;
+                uni = union(rectPos,rectDef);
+                [minY,minX] = find(imPos==min(uni));
+                [maxY,maxX] = find(imPos==max(uni));
+                winFin = struct('x',minX,'y',minY,'w',maxX-minX+1,'h',maxY-minY+1);
+                windowCandidates(winDef)=winFin;
+                break;
+            end
+        end
+        if(new == 1)
+            windowCandidates = [windowCandidates windows(winPos)];
         end
     end
-    if(new == 1)
-        windowCandidates = [windowCandidates windows(winPos)];
-    end
-end  
+end
 
 %Generate the final mask
 mask = im*0;
 for winDef=1:length(windowCandidates)
     mask(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1) = im(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1);
-end
-
-mask2 = im*0;
-for winDef=1:length(windows)
-    mask2(windows(winDef).y:windows(winDef).y+windows(winDef).h-1,windows(winDef).x:windows(winDef).x+windows(winDef).w-1) = im(windows(winDef).y:windows(winDef).y+windows(winDef).h-1,windows(winDef).x:windows(winDef).x+windows(winDef).w-1);
 end
 
 end
