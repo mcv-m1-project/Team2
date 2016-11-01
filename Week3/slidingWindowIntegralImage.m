edd7dc57932cc811eb4472291d14482138f875d9
LidiaGarrucho <lidiaxu.3@gmail.com>
2016-11-01 14:03:08 +0100

    Added needed files for week4 tasks
    Solve bug from previous week in sliding window training

--- Week3/slidingWindowIntegralImage.m
+++ Week3/slidingWindowIntegralImage.m
@@ -24,20 +24,39 @@ for n=1:stepH:N-height
     end
 end
 
-if(size(candidates,2)~=0)
-    
-    [selectedCandidates,selectedScore] = selectStrongestBbox(candidates,score);
-    for winDef=1:size(selectedCandidates,1)
-        windowCandidates = [windowCandidates, struct('x',int32(selectedCandidates(winDef,1)),'y',int32(selectedCandidates(winDef,2)),'w',int32(selectedCandidates(winDef,3)),'h',int32(selectedCandidates(winDef,4)))];
+%Delete overlapped detections (union)
+imPos = 1:M*N;
+imPos = reshape(imPos,[N,M]);
+windowCandidates = [];
+if(0==isempty(windows))
+    windowCandidates = windows(1);
+    for winPos=2:length(windows)
+        new = 1;
+        for winDef=1:length(windowCandidates)
+            rectPos = imPos(windows(winPos).y:windows(winPos).y+windows(winPos).h-1,windows(winPos).x:windows(winPos).x+windows(winPos).w-1);
+            rectDef = imPos(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1);
+            int = intersect(rectPos,rectDef);
+            if 0==isempty(int) %No empty means that both evaluated windows are overlapped
+                new = 0;
+                uni = union(rectPos,rectDef);
+                [minY,minX] = find(imPos==min(uni));
+                [maxY,maxX] = find(imPos==max(uni));
+                winFin = struct('x',minX,'y',minY,'w',maxX-minX+1,'h',maxY-minY+1);
+                windowCandidates(winDef)=winFin;
+                break;
+            end
+        end
+        if(new == 1)
+            windowCandidates = [windowCandidates windows(winPos)];
+        end
     end
+end
+
     %Generate the final mask
      mask = im*0;
     for winDef=1:length(windowCandidates)
         mask(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1) = im(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1);
         %mask(windowCandidates(winDef,2):windowCandidates(winDef,2)+windowCandidates(winDef,4)-1,windowCandidates(winDef,1):windowCandidates(winDef,1)+windowCandidates(winDef,3)-1) = im(windowCandidates(winDef,2):windowCandidates(winDef,2)+windowCandidates(winDef,4)-1,windowCandidates(winDef,1):windowCandidates(winDef,1)+windowCandidates(winDef,3)-1);
     end
-else
-     mask = im*0;
-end
 
 end
---
+++ Week4/get_signals_main_parameters.m
@@ -0,0 +1,36 @@
+function [signals, signals_list, nrepetitions, min_size, max_size, form_factor, filling_ratio, fr_means, ff_means] = get_signals_main_parameters(dirTrainDataSet, dirgt, dirmask)
+
+addpath('..\Week1\')
+addpath('..\Week2\')
+addpath('..\evaluation\')
+% Create lists with the ground truth annotations files, the mask files, and
+% the original image files:
+[gt_list, mask_list, image_list] = create_files_list(dirTrainDataSet);
+
+% We read the dataset, creating a vector of objects with all the
+% information about each signal:
+signals = create_signal_objects(dirgt, gt_list, dirmask, mask_list, dirTrainDataSet, image_list);
+
+% To handle more easily the filling_ratio and the form_factor, we put them
+% in two vectors. By the way, we detect the signals that have no mask.
+[filling_ratio, form_factor] = get_fr_ff(signals);
+
+% We just consider the signals which have a well defined mask:
+signals = signals(filling_ratio ~= 0);
+
+% See the different types of signals, and count how many times they appear:
+[signals_list, nrepetitions] = count_signals_types(signals);
+
+% Again, create vectors with filling_ratio and form_factor.
+% This time, we also calculate the means within each type of signal:
+[filling_ratio, form_factor, fr_means, ff_means] = get_fr_ff_bytypes(signals, ...
+                                                   signals_list, nrepetitions);
+
+% Calculate minimum and maximum size for each type of signal:
+[min_size, max_size] = get_min_max_size_bytypes(signals, signals_list, nrepetitions);
+
+save('signals_main_parameters', 'signals', 'signals_list', 'nrepetitions', 'fr_means', 'ff_means', 'min_size', 'max_size', 'form_factor', 'filling_ratio');
+
+end
+
+
