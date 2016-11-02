a4cb1767358ccaa8ee052a756d284e93cb50e420
cristinabustos16 <mcb9216@gmail.com>
2016-11-02 18:48:50 +0100

    little change in paths

--- Week4/proba1.m
+++ Week4/proba1.m
@@ -44,15 +44,18 @@ close all
 threshold = [0.1, 0.25];
 sigma = 2;
 
-imagefile = 'C:\Users\Xian\Documents\MCV\M1_IHCV\Proxecto\train\00.001527.jpg';
-imagefile = 'C:\Users\Xian\Documents\MCV\M1_IHCV\Proxecto\train\00.004817.jpg';
-imagefile = 'C:\Users\Xian\Documents\MCV\M1_IHCV\Proxecto\train\00.004909.jpg';
-imagefile = 'C:\Users\Xian\Documents\MCV\M1_IHCV\Proxecto\train\00.005912.jpg';
-imagefile = 'C:\Users\Xian\Documents\MCV\M1_IHCV\Proxecto\train\01.000946.jpg';
-imagefile = 'C:\Users\Xian\Documents\MCV\M1_IHCV\Proxecto\train\01.001524.jpg';
+imagefile = '..\..\train\00.001527.jpg';
+% imagefile = '..\..\train\00.004817.jpg';
+% imagefile = '..\..\train\00.004909.jpg';
+% imagefile = '..\..\train\00.005912.jpg';
+% imagefile = '..\..\train\01.000946.jpg';
+% imagefile = '..\..\train\01.001524.jpg';
 
 image = double(imread(imagefile));
 image_bw = (image(:,:,1) + image(:,:,2) + image(:,:,3)) / (3 * 255);
+% image = imread(imagefile);
+% image_bw=rgb2gray(image);
+
 image_edges = edge(image_bw, 'canny', threshold, 'both', sigma);
 
 figure()
