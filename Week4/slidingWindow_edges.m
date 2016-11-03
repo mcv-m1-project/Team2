2fcf01f03384d617b8f2210eecd4973f1e1583d5
cristinabustos16 <mcb9216@gmail.com>
2016-11-03 04:23:13 +0100

    hace un recorrido desde un tamaño de ventana inicial, hasta uno final, variando de 30 px, con un offset de 10%, verifica que el tamaño de ventana actual sea acorde con los limites del form factor, finalmente para cada ventana guarda un .mat con sus windows candidates

--- Week4/slidingWindow_edges.m
+++ Week4/slidingWindow_edges.m
@@ -1,4 +1,4 @@
-function [mask, windowCandidates] = slidingWindow_edges(image_edges, width0, height0, stepW0, stepH0, sizesrange)
+function [mask, windowCandidates] = slidingWindow_edges(image_edges, width, height)
 
 % Initializing structures:
 windowCandidates = [];
@@ -18,74 +18,73 @@ load('edgesModels.mat')
 templates = cell(4);
 
 %define some threshold detect for testing
-thresholdDT = 500;
+thresholdDT = 1000;
 
 %calculate distance transform of edge's image
-bwdist(image_edges);
+image_dt=bwdist(image_edges);
 
 % Loop over sizes:
-for sizefactor = 1:sizesrange
-    % Resizing width and height:
-    height = round(height0 * sizefactor);
-    width = round(width0 * sizefactor);
-    % Resizing the steps:
-    stepW = round(stepW0 * sizefactor);
-    stepH = round(stepH0 * sizefactor);
-    
-    % Adjust size of models to fit the windows:
-    templates{1} = imresize(circleEdges, [height, width]);
-    templates{2} = imresize(squareEdges, [height, width]);
-    templates{3} = imresize(upTriangleEdges, [height, width]);
-    templates{4} = imresize(downTriangleEdges, [height, width]);
+% for sizefactor = sizesrange
+% Resizing width and height:
+% height = round(height0 * sizefactor);
+% width = round(width0 * sizefactor);
+% % Resizing the steps:
+% stepW = round(stepW0 * sizefactor);
+% stepH = round(stepH0 * sizefactor);
 
-    % Trying all windows over the image:
-    for n = 1 : stepH : N-height+1
-        for m = 1 : stepW : M-width+1
-            % Content of the image in the window:
-            subIm = image_edges(n : min(N, n+height-1), m : min(M,m+width-1));
+%the window step is 10% of the size of window
+stepH = round(0.1*height);
+stepW = round(0.1*width);
 
-            % We try with the four different signals shapes:
-            % (circle, square, and up and down triangles)
-            minWindowScore = 0;
-            for signal_shape = 1:4
-                % Sum of product of the Distance Transform and the edges in 
-                % the window:
-                windowScore = sum(sum(templates{signal_shape} .* subIm));
-                
-                % Maxmum score between the different shapes:
-                if windowScore>0
-                    minWindowScore = min(minWindowScore, windowScore);
-                end
-            end
+% Adjust size of models to fit the windows:
+templates{1} = imresize(circleEdges, [height, width],'nearest');
+templates{2} = imresize(squareEdges, [height, width],'nearest');
+templates{3} = imresize(upTriangleEdges, [height, width],'nearest');
+templates{4} = imresize(downTriangleEdges, [height, width],'nearest');
 
+% Trying all windows over the image:
+for n = 1 : stepH : N-height+1
+    for m = 1 : stepW : M-width+1
+        % Content of the image in the window:
+        subIm = image_dt(n : min(N, n+height-1), m : min(M,m+width-1));
+        % We try with the four different signals shapes:
+        % (circle, square, and up and down triangles)
+        minWindowScore = 100000000;
+        for signal_shape = 1:4
+            % Sum of product of the Distance Transform and the edges in
+            % the window:
+            windowScore = sum(sum(templates{signal_shape} .* subIm));
+            % Minimum score between the different shapes:
+            minWindowScore = min(minWindowScore, windowScore);
+        end
+        
+        % Deciding if the window is a candidate:
+        if(minWindowScore < thresholdDT)
             
-            % Deciding if the window is a candidate:
-            if(minWindowScore < thresholdDT)
-                minWindowScore
-                windows = [windows, struct('x', m, 'y', n, 'w', width, 'h', height)];
-                candidates = [candidates; m, n, width, height];
-                %score = [score; filling_ratio];
-                score = [score; minWindowScore];
-            end
+            windows = [windows, struct('x', m, 'y', n, 'w', width, 'h', height)];
+            candidates = [candidates; m, n, width, height];
+            %score = [score; filling_ratio];
+            score = [score; minWindowScore];
         end
     end
 end
+% end
 
 %draw windows candidates
-img_candidates = image_edges;
-figure()
-imshow(img_candidates, [0 1])
-for i=1:size(windows,1)
-    hold on
-    x = windows(i).x;
-    y = windows(i).y;
-    w = windows(i).w;
-    h = windows(i).h;
-    plot([x x], [y y+h], 'y')
-    plot([x+w x+w], [y y+h], 'y')
-    plot([x x+w], [y y], 'y')
-    plot([x x+w], [y+h y+h], 'y')
-end
+% img_candidates = image_edges;
+% figure();
+% imshow(img_candidates,[0 1]);
+% for i=1:size(windows,2)
+%     hold on
+%     x = windows(i).x;
+%     y = windows(i).y;
+%     w = windows(i).w;
+%     h = windows(i).h;
+%     plot([x x], [y y+h], 'y')
+%     plot([x+w x+w], [y y+h], 'y')
+%     plot([x x+w], [y y], 'y')
+%     plot([x x+w], [y+h y+h], 'y')
+% end
 
 
 % Delete overlapped detections (union)
@@ -117,18 +116,21 @@ if(0==isempty(windows))
 end
 
 % if(size(candidates,2)~=0)
-    
+
 %     [selectedCandidates,selectedScore] = selectStrongestBbox(candidates,score);
 %     for winDef=1:size(selectedCandidates,1)
 %         windowCandidates = [windowCandidates, struct('x',int32(selectedCandidates(winDef,1)),'y',int32(selectedCandidates(winDef,2)),'w',int32(selectedCandidates(winDef,3)),'h',int32(selectedCandidates(winDef,4)))];
 %     end
-    %Generate the final mask
-     mask = image_edges*0;
-    for winDef=1:length(windowCandidates)
-        mask(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1) = image_edges(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1);
-        %mask(windowCandidates(winDef,2):windowCandidates(winDef,2)+windowCandidates(winDef,4)-1,windowCandidates(winDef,1):windowCandidates(winDef,1)+windowCandidates(winDef,3)-1) = im(windowCandidates(winDef,2):windowCandidates(winDef,2)+windowCandidates(winDef,4)-1,windowCandidates(winDef,1):windowCandidates(winDef,1)+windowCandidates(winDef,3)-1);
-    end
 
+
+%Generate the final mask
+mask = image_edges*0;
+for winDef=1:length(windowCandidates)
+    mask(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1) = image_edges(windowCandidates(winDef).y:windowCandidates(winDef).y+windowCandidates(winDef).h-1,windowCandidates(winDef).x:windowCandidates(winDef).x+windowCandidates(winDef).w-1);
+    %mask(windowCandidates(winDef,2):windowCandidates(winDef,2)+windowCandidates(winDef,4)-1,windowCandidates(winDef,1):windowCandidates(winDef,1)+windowCandidates(winDef,3)-1) = im(windowCandidates(winDef,2):windowCandidates(winDef,2)+windowCandidates(winDef,4)-1,windowCandidates(winDef,1):windowCandidates(winDef,1)+windowCandidates(winDef,3)-1);
+end
+
+return;
 end
 
 
--- Week4/week4_task2_global.m
+++ Week4/week4_task2_global.m
@@ -2,29 +2,35 @@ clearvars
 close all
 
 % Computing Distance Transforms of model signals:
-load('edgesModels.mat')
-circleDT = bwdist(circleEdges);
-squareDT = bwdist(squareEdges);
-upTriangleDT = bwdist(upTriangleEdges);
-downTriangleDT = bwdist(downTriangleEdges);
-save('DTModels.mat', 'circleDT', 'squareDT', 'upTriangleDT', 'downTriangleDT')
-figure()
-subplot(2,4,1)
-imshow(circleEdges)
-subplot(2,4,2)
-imshow(circleDT, [0, max(max(circleDT))])
-subplot(2,4,3)
-imshow(squareEdges)
-subplot(2,4,4)
-imshow(squareDT, [0, max(max(squareDT))])
-subplot(2,4,5)
-imshow(upTriangleEdges)
-subplot(2,4,6)
-imshow(upTriangleDT, [0, max(max(upTriangleDT))])
-subplot(2,4,7)
-imshow(downTriangleEdges)
-subplot(2,4,8)
-imshow(downTriangleDT, [0, max(max(downTriangleDT))])
+% load('edgesModels.mat')
+% circleDT = bwdist(circleEdges);
+% squareDT = bwdist(squareEdges);
+% upTriangleDT = bwdist(upTriangleEdges);
+% downTriangleDT = bwdist(downTriangleEdges);
+% save('DTModels.mat', 'circleDT', 'squareDT', 'upTriangleDT', 'downTriangleDT')
+% figure()
+% subplot(2,4,1)
+% imshow(circleEdges)
+% subplot(2,4,2)
+% imshow(circleDT, [0, max(max(circleDT))])
+% subplot(2,4,3)
+% imshow(squareEdges)
+% subplot(2,4,4)
+% imshow(squareDT, [0, max(max(squareDT))])
+% subplot(2,4,5)
+% imshow(upTriangleEdges)
+% subplot(2,4,6)
+% imshow(upTriangleDT, [0, max(max(upTriangleDT))])
+% subplot(2,4,7)
+% imshow(downTriangleEdges)
+% subplot(2,4,8)
+% imshow(downTriangleDT, [0, max(max(downTriangleDT))])
+
+%load main parameters 
+load('signals_main_parameters');
+%get the max and min form factor
+min_ff = min(form_factor);
+max_ff = max(form_factor);
 
 % Parameters for Canny edge detector:
 threshold = [0.1, 0.25];
@@ -47,8 +53,32 @@ for i = 1 : size(imageslist,1)
     % Computing edges:
     image_edges = edge(image_grey, 'canny', threshold, 'both', sigma);
     
+    % testing values, they were chosen for intuition
+    width_inicial = 60;%hand picked
+    height_inicial = 60;
+    
+    width_final = 400;%hand picked
+    height_final = 400;
+    
+    stepW = 30;
+    stepH = 30;
+    
+    offsetWidth=30;
+    offsetHeight=30;
     % Sweep templates across image:
-    [mask, windowCandidates] = slidingWindow_edges(image_edges, width, height, stepW, stepH, sizes);
+    
+    for width = width_inicial: offsetWidth : width_final
+        for height = height_inicial : offsetHeight : height_final
+            
+            current_form_factor=width/height;%verify if window's form factor is between my limits
+            if min_ff<current_form_factor & current_form_factor<max_ff
+                [mask, windowCandidates] = slidingWindow_edges(image_edges, width, height);
+                %write window candidates
+                file_name_result=strcat('windowCandidate',num2str(width),'x',num2str(height),'.mat');
+                save(file_name_result,'windowCandidates');
+            end
+        end
+    end
 end
 
 
